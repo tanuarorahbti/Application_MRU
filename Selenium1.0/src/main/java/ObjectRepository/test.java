6a3908b84245c047971d70ebd5dbb850452b007e
LAPPY <LAPPY@DELL>
2019-01-16 05:50:42 -0800

    first commit

---
+++ Selenium1.0/src/main/java/ObjectRepository/test.java
@@ -0,0 +1,23 @@
+package ObjectRepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+	public  static void main(String[] args) {
+		login();
+		
+	}
+	public static void login() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\LAPPY\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://facebook.com");
+	WebElement loginbtn = driver.findElement(By.xpath("//input[@value = 'Log In']"));
+	loginbtn.click();
+	
+	}
+	
+
+}
